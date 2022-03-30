0d23fb6e4be5b4998cd7a0633e34c5c776659d8f
Yadu Krishnan <yadavan88@gmail.com>
2022-03-30 16:27:40 +0200

    added sample munit test to template

---
+++ src/main/g8/src/test/scala/$package$/app/SampleSpec.scala
@@ -0,0 +1,9 @@
+package $package$.app
+
+class SampleSpec extends munit.FunSuite {
+    test("sample test") {
+        val input = 100
+        val output = 100
+        assertEquals(input, output)
+    }
+}
